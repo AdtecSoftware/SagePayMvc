31655991dd6427c6a33082c5bfb80e1fbc2e31e9
George Hemmings <gh@adtecsoftware.com>
2015-09-17 12:52:04 +0100

    Repackage for temporary forked version of project

--- build.bat
+++ build.bat
@@ -4,9 +4,6 @@ if "%1"=="PublishPackages" goto publish
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj %*
 goto end
 
-:publish
-powershell.exe -noprofile ./publish-nuget-packages.ps1
-
 goto end
 
 :end
